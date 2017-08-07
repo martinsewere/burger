e57bb2334a9845b006f2a94128c823ee6d64a58b
martinsewere <martinsewere@gmail.com>
2017-08-07 16:20:18 -0700

    burgerJS and handlebars

---
+++ models/burger.js
@@ -0,0 +1,37 @@
+var orm = require("../config/orm.js");
+
+var burger = {
+
+	all: function(callBack) {
+
+		orm.all("burgers", function(burgers){
+		callBack(burgers);
+
+		});
+	},
+
+	create: function(cols, vals, callBack) {
+    orm.create("burgers", cols, vals, function(res) {
+      callBack(res);
+    });
+  }, 
+
+  	update: function(objColVals, state, callBack) {
+
+  	orm.update("burgers",objColVals, state, function(res) {
+
+  		callBack(res);
+
+  	});
+
+  	},
+
+  	delete: function(condition, cb) {
+    orm.delete("burgers", condition, function(res) {
+      cb(res);
+    });
+  }
+
+};
+
+module.exports = burger;
